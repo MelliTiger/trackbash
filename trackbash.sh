6275474b144284e1011f6fac9ccf42d094172991
MelliTiger <adp@adop.de>
2019-12-28 15:53:23 +0100

    Erste veroeffentlichte Version

---
+++ trackbash.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+logdatei=$HOME"/"$( date -u +%Y-%m-%d_%H-%M-%S-%Z)".csv"
+echo "Skript zum Auslesen der DB API"
+echo "--"
+echo "Speichere in "$logdatei
+
+# Feste Daten abrufen
+status=$(curl -sf https://iceportal.de/api1/rs/status | jq . )
+train_type=$(echo "$status" | jq -r '.trainType') # Zugtyp
+train_series=$(echo "$status" | jq -r '.series')  # Baureihe
+train_train=$(echo "$status" | jq -r '.tzn')     # Triebzugnummer
+gps_status=$(echo "$status" | jq -r '.gpsStatus')
+
+throttle=1
+
+while true 
+do
+   if [ $throttle -eq "1" ]
+     then
+       train=$(curl -sf https://iceportal.de/api1/rs/tripInfo/trip | jq '.')
+       train_number=$(echo "$train" | jq -r '.trip.vzn')
+       train_dst_total=$(echo "$train" | jq -r '.trip.totalDistance')
+       train_dst_total=$((train_dst_total / 1000))
+       train_dst_last=$(echo "$train" | jq -r '.trip.distanceFromLastStop')
+       train_dst_last=$((train_dst_last / 1000))
+       station=$(curl -sf https://iceportal.de/api1/rs/tripInfo/trip | jq '[.[].stops[]? | select(.info.passed == false)][0]')
+       station_name=$(echo "$station" | jq -r '.station.name')
+       station_track=$(echo "$station" | jq -r '.track.actual')
+       station_arrival=$(echo "$station" | jq -r '.timetable.scheduledArrivalTime')
+       station_arrival=$(date --date="@$((station_arrival / 1000))" +%H:%M)
+       station_delay=$(echo "$station" | jq -r '.timetable.arrivalDelay')
+       if [ -n "$station_delay" ];
+         then
+           station_delay=$station_delay
+         else
+           station_delay="pünktlich"
+         fi
+	throttle=6
+     fi
+   printf "\033c"
+   echo "Zug "$train_type" "$train_number" mit Baureihe "$train_series", Triebzug "$train_train". GPS-Status: "$gps_status
+   echo "Gesamte Strecke: "$train_dst_total" km, seit letztem Halt: "$train_dst_last" km"
+   echo "Nächster Halt: "$station_name" auf Gleis "$station_track" um "$station_arrival". Aktuelle Verspätung: "$station_delay
+   echo "---  Speicherung unter : "$logdatei
+   status=$(curl -sf https://iceportal.de/api1/rs/status | jq .  ) 
+   gps_latitude=$(echo "$status" | jq -r '.latitude')
+   gps_longitude=$(echo "$status" | jq -r '.longitude')
+   gps_speed=$(echo "$status" | jq -r '.speed')
+   gps_zeit=$(echo "$status" | jq -r '.serverTime')
+   gps_status=$(echo "$status" | jq -r '.gpsStatus')
+   case "$gps_status" in
+     VALID)
+        echo $gps_zeit", "$gps_latitude", "$gps_longitude", "$gps_speed >> $logdatei
+        ;;
+     LAST_KNOWN_POSITION)
+        echo "#"$gps_zeit", Kein Empfang, Kein Empfang, "$gps_speed >> $logdatei
+        ;;
+     *)
+        echo "#"$gps_zeit", unklare Rückmeldung!"
+        ;;
+   esac     
+   tail -n $((LINES - 7)) $logdatei | column --table -s, --table-columns Zeit,Breitengrad,Längengrad,Geschwindigkeit
+   throttle=$((throttle-1))
+   sleep 5
+done
