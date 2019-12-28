df43bfa91c60ea3a370ab5041bf06ef0a9d7edd0
MelliTiger <adp@adop.de>
2019-12-28 16:01:21 +0100

    Lizenztext in Source

--- trackbash.sh
+++ trackbash.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
+# Abfrage der API im Zug, Ausgabe der Daten und mitschreiben des GPS-Logs
+# Copyright (C) 2019 Andreas Preissig
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+##
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 
 logdatei=$HOME"/"$( date -u +%Y-%m-%d_%H-%M-%S-%Z)".csv"
 echo "Skript zum Auslesen der DB API"
